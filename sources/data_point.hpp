b4d051c0eadee492ff1dae49dae500d973919ac0
KGergo88 <kocsisandrasgergo@gmail.com>
2018-07-15 17:42:24 +0200

    Adding the lincense headline to the sources and correcting a typo in the README

--- sources/data_point.hpp
+++ sources/data_point.hpp
@@ -1,9 +1,25 @@
-/* 
- * File:   data_point.hpp
- * Author: gergo
- *
- * Created on July 16, 2017, 7:23 PM
- */
+//==============================================================================//
+//                                                                              //
+//    RDB Diplomaterv Monitor                                                   //
+//    A monitor program for the RDB Diplomaterv project                         //
+//    Copyright (C) 2018  András Gergő Kocsis                                   //
+//                                                                              //
+//    This program is free software: you can redistribute it and/or modify      //
+//    it under the terms of the GNU General Public License as published by      //
+//    the Free Software Foundation, either version 3 of the License, or         //
+//    (at your option) any later version.                                       //
+//                                                                              //
+//    This program is distributed in the hope that it will be useful,           //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+//    GNU General Public License for more details.                              //
+//                                                                              //
+//    You should have received a copy of the GNU General Public License         //
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.    //
+//                                                                              //
+//==============================================================================//
+
+
 
 #include "global.hpp"
 
