a16e6a921060d40f18f423940bd68f5bc88a83b4
András Gergö Kocsis <kocsisandrasgergo@gmail.com>
2020-12-21 01:07:26 +0100

    Updating the Ubuntu job and adding the base of the Windows job
    Details:
     - Renaming the Ubuntu job
     - Reporting the coverage report in the Ubuntu job
     - Reworking the tests/test_coverage.py
     - Adding the Windows job with the checkout and Install Qt steps

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,38 +1,45 @@
 import os
 import sys
 import subprocess
+import argparse
 
 
-folder_build = os.path.join(os.path.dirname(__file__), "..", "build", "tests")
-folder_results = os.path.join(folder_build, "coverage_reports")
-path_test_executable = os.path.join(folder_build, "RDB_Diplomaterv_Monitor_Unit_Tests")
-path_coverage_file = os.path.join(folder_results, "coverage.info")
-
-
-def main():
-    # Changing the wokring folder to this script's folder
-    os.chdir(sys.path[0])
-
+def main(args):
     # Running the tests so that the .gcno and .gcov files get generated
-    subprocess.run([path_test_executable])
+    subprocess.run([args.test_executable])
 
-    # Creating the results folder if it not yet exists
-    if not os.path.isdir(folder_results):
-        print("Creating the results folder (" + folder_results + ")...")
-        os.mkdir(folder_results)
+    # Setting the results folder
+    results_folder = os.getcwd()
+    if args.output_directory is not None:
+        results_folder = args.output_directory
+
+    # Creating the results folder if it does not exists
+    if not os.path.isdir(results_folder):
+        print("Creating the results folder (" + results_folder + ")")
+        os.mkdir(results_folder)
+
+    # The path to the file containing the coverage info
+    coverage_file = os.path.join(results_folder, "coverage.info")
 
     # Generate the coverage file from the .gcno and .gcov files
-    subprocess.run(["lcov", "-d", folder_build, "-c", "-o", path_coverage_file])
+    subprocess.run(["lcov", "-d", os.path.dirname(args.test_executable), "-c", "-o", coverage_file])
 
     # Filtering the coverage file: Remove some paths/files we don't want to calculate the code coverage
-    subprocess.run(["lcov", "--remove", path_coverage_file, "/usr/include/*", "*/googlemock/*", "*/googletest/*", "*/QtCore/*", "-o", path_coverage_file])
+    subprocess.run(["lcov", "--remove", coverage_file, "/usr/include/*", "*/googlemock/*", "*/googletest/*", "*/QtCore/*", "-o", coverage_file])
 
     # Generate the HTML files from the coverage file
-    subprocess.run(["genhtml", "-o", folder_results, path_coverage_file])
+    subprocess.run(["genhtml", "-o", results_folder, coverage_file])
  
     # Reset our counts
-    subprocess.run(["lcov", "-d", folder_results, "-z"])
+    subprocess.run(["lcov", "-d", results_folder, "-z"])
 
 
 if "__main__" == __name__:
-    main()
+    parser = argparse.ArgumentParser(
+        prog="test_coverage.py",
+        description="Script to run the unit tests and generate the coverage reports."
+    )
+    parser.add_argument("-t", "--test_executable", type=str, required=True, help="Path of the unit test executable.")
+    parser.add_argument("-o", "--output_directory", type=str, required=False, help="Path of the folder where the results shall be put. Defaults to the working directory if left empty.")
+    args = parser.parse_args()
+    main(args)
