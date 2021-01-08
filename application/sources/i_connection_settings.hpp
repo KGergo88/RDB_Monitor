13d78f46459f86a14bf7c3d4d81f773607c2fdd1
András Gergö Kocsis <kocsisandrasgergo@gmail.com>
2021-01-08 23:21:50 +0100

    Adding the I_ConnectionSettings and the SerialPortSettings

---
+++ application/sources/i_connection_settings.hpp
@@ -0,0 +1,41 @@
+//==============================================================================//
+//                                                                              //
+//    RDB Diplomaterv Monitor                                                   //
+//    A monitor program for the RDB Diplomaterv project                         //
+//    Copyright (C) 2018  András Gergő Kocsis                                   //
+//                                                                              //
+//    This program is free software: you can redistribute it and/or modify      //
+//    it under the terms of the GNU General Public License as published by      //
+//    the Free Software Foundation, either version 3 of the License, or         //
+//    (at your option) any later version.                                       //
+//                                                                              //
+//    This program is distributed in the hope that it will be useful,           //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+//    GNU General Public License for more details.                              //
+//                                                                              //
+//    You should have received a copy of the GNU General Public License         //
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.    //
+//                                                                              //
+//==============================================================================//
+
+
+
+#include <QString>
+
+
+
+#ifndef I_CONNECTION_SETTINGS_HPP
+#define I_CONNECTION_SETTINGS_HPP
+
+
+
+class I_ConnectionSettings
+{
+public:
+    virtual ~I_ConnectionSettings() = default;
+};
+
+
+
+#endif // I_CONNECTION_SETTINGS_HPP
---
+++ application/sources/serial_port_settings.hpp
@@ -0,0 +1,72 @@
+//==============================================================================//
+//                                                                              //
+//    RDB Diplomaterv Monitor                                                   //
+//    A monitor program for the RDB Diplomaterv project                         //
+//    Copyright (C) 2018  András Gergő Kocsis                                   //
+//                                                                              //
+//    This program is free software: you can redistribute it and/or modify      //
+//    it under the terms of the GNU General Public License as published by      //
+//    the Free Software Foundation, either version 3 of the License, or         //
+//    (at your option) any later version.                                       //
+//                                                                              //
+//    This program is distributed in the hope that it will be useful,           //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+//    GNU General Public License for more details.                              //
+//                                                                              //
+//    You should have received a copy of the GNU General Public License         //
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.    //
+//                                                                              //
+//==============================================================================//
+
+
+
+#include <QSysInfo>
+#include <QSerialPort>
+
+#include "i_connection_settings.hpp"
+
+
+
+#ifndef SERIAL_PORT_SETTINGS_HPP
+#define SERIAL_PORT_SETTINGS_HPP
+
+
+
+static constexpr char serial_port_connection_name[] = "SerialPort";
+
+class SerialPortSettings : public I_ConnectionSettings
+{
+public:
+    SerialPortSettings()
+    {
+        auto os_name = QSysInfo::productType();
+
+        if(("winrt" == os_name) && ("windows" == os_name))
+        {
+            portName = "COM3";
+        }
+        else
+        {
+            portName = "/dev/ttyACM0";
+        }
+        baudRate = QSerialPort::BaudRate::Baud115200;
+        dataBits = QSerialPort::DataBits::Data8;
+        stopBits = QSerialPort::StopBits::OneStop;
+        parity = QSerialPort::Parity::NoParity;
+        flowControl = QSerialPort::FlowControl::NoFlowControl;
+    }
+
+    virtual ~SerialPortSettings() {};
+
+    QString portName;
+    QSerialPort::BaudRate baudRate;
+    QSerialPort::DataBits dataBits;
+    QSerialPort::StopBits stopBits;
+    QSerialPort::Parity parity;
+    QSerialPort::FlowControl flowControl;
+};
+
+
+
+#endif // SERIAL_PORT_SETTINGS_HPP
